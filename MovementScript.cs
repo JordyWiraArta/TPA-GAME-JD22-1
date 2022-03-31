e971ac3ee01caeacbd05486f10825f2326836137
JordyWiraArta <98179507+JordyWiraArta@users.noreply.github.com>
2022-03-31 13:59:17 +0700

    update movement script with jump

--- MovementScript.cs
+++ MovementScript.cs
@@ -5,43 +5,69 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     private Animator animator;
-    [SerializeField] private Camera camField;
     [SerializeField] private float turnSpeed;
-    private CharacterController playerController;
+    [SerializeField] private CharacterController playerController;
 
-    [SerializeField] float groundY;
-    [SerializeField] LayerMask groundMaskLayer;
-    Vector3 spherePos;
+    [SerializeField] private float groundY;
+    [SerializeField] private LayerMask groundMaskLayer;
+    [SerializeField] private float jumpSpeed, directionalSpeed;
 
     private float gravity = -9.8f;
     private Vector3 velocity;
+
+    private bool isJump;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        setGravity();
+        velocity.y = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Pos X", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Pos Y", Input.GetAxis("Vertical"));
-  
-    }
+        if (velocity.y <0 && isGrounded())
+        {
+            velocity.y = 0;
+        }
 
-    bool isGrounded()
-    {
-        spherePos = new Vector3(transform.position.x, transform.position.y - groundY, transform.position.z);
-        if (Physics.CheckSphere(spherePos, playerController.radius - 0.1f, groundMaskLayer)) return true;
-        else return false;
-    }
+        
+        if (isGrounded() == true)
+        {
+            animator.SetFloat("Pos X", Input.GetAxis("Horizontal"));
+            animator.SetFloat("Pos Y", Input.GetAxis("Vertical"));
 
-    void setGravity()
-    {
-        if (!isGrounded()) velocity.y += gravity * Time.deltaTime;
+            animator.SetBool("ISJUMP", false);
 
+            if (Input.GetButtonDown("Jump"))
+            {
+                isJump = true;
+                animator.SetBool("ISJUMP", true);
+            }
+        }
+        else if (!isJump && isGrounded() == false)
+        {
+            animator.SetBool("ISJUMP", true);
+        }
+            
+        if(isJump)
+        {
+            velocity.y = Mathf.Sqrt(jumpSpeed * -2 * gravity);
+            isJump = false;
+        }
+
+        velocity.y += (gravity * Time.deltaTime);
         playerController.Move(velocity * Time.deltaTime);
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics.CheckSphere(transform.position, groundY, groundMaskLayer))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
